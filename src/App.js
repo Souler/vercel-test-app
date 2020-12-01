ba76c728112fbc77e3ca3494a3842a1df69bdd95
Souler <juanjoseherrero@playtomic.io>
2020-12-01 08:42:18 +0100

    dummy change

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <div><code>{A}</code></div>
           <div><code>{B}</code></div>
         </p>
-        <div>6</div>
+        <div>7</div>
       </header>
     </div>
   );
